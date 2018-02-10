271d74801f34f4ee62f2665556e12ad2ccbff61b
Mohnish G J <mail4mohnish@gmail.com>
2018-02-10 14:40:58 +0800

    Add the ability to generate a sales receipt based on a quantity of 'n' items per product

--- bin/generate_sales_receipt.rb
+++ bin/generate_sales_receipt.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/csv_parser'
 require_relative '../lib/line_item'
+require_relative '../lib/shopping_cart'
+require_relative '../lib/receipt_generator'
+require_relative '../lib/tax_calculator'
 
 file_name = 'input/shopping_basket2.csv'
 
@@ -8,3 +11,6 @@ products = CsvParser.new(file_name).read
 line_items = products.each_with_object([]) do |line_item, items|
                items << LineItem.new(line_item["Product"], BigDecimal.new(line_item["Price"]), line_item["Quantity"].to_i)
              end
+
+shopping_cart = ShoppingCart.new(line_items, TaxCalculator.new, ReceiptGenerator.new)
+shopping_cart.purchase_items
---
+++ spec/lib/shopping_cart_spec.rb
@@ -0,0 +1,29 @@
+describe ShoppingCart do
+  describe '#purchase_items' do
+    context 'given a list of items, a tax calculator and a receipt generator' do
+      it 'calculates details related to the items in a cart and generates a sales receipt' do
+        file_name = 'input/shopping_basket3.csv'
+        line_items = CsvParser.new(file_name).read
+        items = line_items.each_with_object([]) do |line_item, items|
+                  items << LineItem.new(line_item["Product"], BigDecimal.new(line_item["Price"]), line_item["Quantity"].to_i)
+                end
+
+        shopping_cart = ShoppingCart.new(items, TaxCalculator.new, ReceiptGenerator.new)
+
+        expect {shopping_cart.purchase_items}.to output(purchase_receipt).to_stdout
+      end
+    end
+  end
+
+  def purchase_receipt
+    <<~PURCHASE_DETAILS
+      1, imported bottle of perfume, 32.19
+      1, bottle of perfume, 20.89
+      1, packet of headache pills, 9.75
+      1, imported box of chocolates, 11.85
+
+      Sales Taxes: 6.7
+      Total: 74.68
+    PURCHASE_DETAILS
+  end
+end
